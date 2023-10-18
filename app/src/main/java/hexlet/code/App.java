4b7d9f59aae72a53b3e3072d93b83ec6b13fb190
Eliza Moritz <dark.little.finch@gmail.com>
2023-10-18 23:25:36 +0700

    add: main class App

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,20 @@
 package hexlet.code;
 
-public class Main {
+import io.javalin.Javalin;
+
+public class App {
+    public static Javalin getApp() {
+        var app = Javalin.create(config -> {
+            config.plugins.enableDevLogging();
+        });
+
+        app.get("/", ctx -> ctx.result("Hello, World!"));
+
+        return app;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Javalin app = getApp();
+        app.start(7070);
     }
 }
